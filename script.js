56465b6b81811f2dc16887f1a1ca7f527fac050d
jfelipnasc <jfelipnasc@gmail.com>
2025-07-08 00:28:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    // Seleciona o container da mensagem
+    var messageContainer = document.getElementById('messageContainer');
+    
+    // Adiciona uma frase de motivação ao clicar
+    messageContainer.textContent = "A curiosidade é o motor do desenvolvimento. Continue criando e aprendendo!";
+});
